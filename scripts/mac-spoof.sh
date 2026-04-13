21e8a157cc5591b0e85cd0996da01ee2b85a9bbb
TheRealShyft <pun_croc@hotmail.com>
2026-04-13 10:52:34 +1000

    Update mac-spoof.sh

--- scripts/mac-spoof.sh
+++ scripts/mac-spoof.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 source config.txt
-read -p "Interface: " interface
+read -p "Interface: (not mon)" int
 
-airmon-ng stop wlan1mon
-ifconfig $interface down
-macchanger -m $client_mac $interface
-ifconfig $interface up
-airmon-ng start $interface
+airmon-ng stop $interface
+ifconfig $int down
+macchanger -m $client_mac $int
+ifconfig $int up
+airmon-ng start $int
